35ae673e61cce3f0c39489aa2e3aa8f3d22a44e7
Ele Morelli <etc+github@elemorelli.com>
2026-04-18 15:22:53 +0100

    refactor: remove deprecated ATTACK_COUNT constant, using MAX_ATTACK_COUNT instead for improved code clarity

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -9,9 +9,6 @@ export const BASE_ATTACK_COUNT = 3;
 /** Bases plus up to one berserker per base (damage-gated). */
 export const MAX_ATTACK_COUNT = 6;
 
-/** @deprecated Use MAX_ATTACK_COUNT */
-export const ATTACK_COUNT = MAX_ATTACK_COUNT;
-
 export const DEF_MIN = 2;
 export const DEF_MAX = 6;
 
